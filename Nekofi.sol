3fbe302536703190c0a7dc50a0437969237131a6
lxgion <marek5lipina@gmail.com>
2022-05-13 17:10:47 +0200

    Initial commit

---
+++ Nekofi.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MPL-2.0
+pragma solidity ^0.8.13;
+
+contract Nekofi {
+    mapping (address => uint256) public balanceOf;
+    mapping (address => uint256) public locked;
+    mapping (address => uint256) public lockStamp;
+
+    string public name = "Nekomimi";
+    string public symbol = "NEKO";
+    uint8 public decimals = 18;
+    address public DEAD = 0x000000000000000000000000000000000000dEaD;
+    address public NekoGuard = 0x0000000000000000000000000000000000000000;
+    address public owner;
+
+    uint256 public totalSupply = 1000000 * (uint256(10) ** decimals);
+
+    constructor() { 
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+        owner = msg.sender;
+    }
+
+    modifier ownerOnly {
+        require(msg.sender == owner);
+        _;
+    }
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function transfer(address to, uint256 value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= value);
+
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    function approve(address spender, uint256 value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool success) {
+        require(value <= balanceOf[from]);
+        require(value <= allowance[from][msg.sender]);
+
+        balanceOf[from] -= value;
+        balanceOf[to] += value;
+        allowance[from][msg.sender] -= value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    function mint(uint256 amount) public ownerOnly returns (bool success) {
+        balanceOf[msg.sender] += amount;
+        emit Transfer(address(0), msg.sender, amount);
+        return true;
+    }
+
+    function 
+
+    receive() payable external {
+        balanceOf[NekoGuard] += msg.value;
+        emit Transfer(msg.sender, NekoGuard, msg.value);
+    }
+}
